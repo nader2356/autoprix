2aabc9997a5ca66497e371cdd9b9cf274a204105
nader2356 <naderabdessaied8@gmail.com>
2023-05-02 23:59:28 +0200

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-
-import './App.css';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import theme from './theme/theme';
+import "./App.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme/theme";
+import Router from "./Route/router";
+import { useRoutes } from "react-router-dom/dist";
 
 function App() {
+ 
   return (
-    <ThemeProvider theme= {theme}>
-    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
 
-
-
-    
-  </ThemeProvider>
+     
+    </ThemeProvider>
   );
 }
 
---
+++ src/Dashboard/Dashboards.js
@@ -0,0 +1,10 @@
+import Box from '@mui/material/Box/Box'
+import Typography from '@mui/material/Typography/Typography'
+
+export default function Dashboards() {
+	return (
+		<Box>
+			<Typography variant='h1'>Dashboard</Typography>
+		</Box>
+	)
+}
